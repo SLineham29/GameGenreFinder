24576b91ec72fb20fea7f82786fff4f9ba42754a
Slineham29 <samuel.lineham@hotmail.com>
2026-09-26 17:53:22 +0100

    Select boxes in the game-finder form are now populated with information from the IGDB on page load.

--- app/Services/IgdbClient.php
+++ app/Services/IgdbClient.php
@@ -7,10 +7,11 @@ use Illuminate\Support\Facades\Http;
 
 class IgdbClient
 {
+    private string $apiUrl = 'https://api.igdb.com/v4';
 
     public function getOAuth(): String
     {
-        if($accessToken = Cache::get('igdb_access_token')){
+        if($accessToken = Cache::get('igdb_access_token')) {
             return $accessToken;
         }
 
@@ -23,5 +24,33 @@ class IgdbClient
         Cache::put('igdb_access_token', $response['access_token'], now()->addSeconds($response['expires_in']));
 
         return $response['access_token'];
+    }
+
+    public function getGenres(): array
+    {
+        return Cache::remember('igdb_genres', now()->addDay(), function () {
+            return Http::withHeaders([
+                'Client-ID' => config('services.igdb.client_id'),
+                'Authorization' => 'Bearer ' . $this->getOAuth(),
+            ])
+            ->withBody('fields id,name; sort name asc; limit 500;', 'text/plain')
+            ->post("{$this->apiUrl}/genres")
+            ->throw()
+            ->json();
+        });
+    }
+
+    public function getPlatforms(): array
+    {
+        return Cache::remember('igdb_platforms', now()->addDay(), function () {
+            return Http::withHeaders([
+                'Client-ID' => config('services.igdb.client_id'),
+                'Authorization' => 'Bearer ' . $this->getOAuth(),
+            ])
+                ->withBody('fields id,name; sort name asc; limit 500;', 'text/plain')
+                ->post("{$this->apiUrl}/platforms")
+                ->throw()
+                ->json();
+        });
     }
 }
--- resources/views/pages/game-finder.blade.php
+++ resources/views/pages/game-finder.blade.php
@@ -1,10 +1,23 @@
 <?php
 
 use Livewire\Component;
+use App\Services\IgdbClient;
 
 new class extends Component
 {
-    //
+    public array $genres = [];
+    public array $platforms = [];
+
+    public ?int $genre1 = null;
+    public ?int $genre2 = null;
+    public ?int $genre3 = null;
+    public ?int $platform = null;
+
+    public function mount(IgdbClient $client): void
+    {
+        $this->genres = $client->getGenres();
+        $this->platforms = $client->getPlatforms();
+    }
 };
 ?>
 
@@ -14,12 +27,28 @@ new class extends Component
 
     <form class="space-y-2">
         <div>
-            <flux:select label="Genre 1"/>
-            <flux:select label="Genre 2"/>
-            <flux:select label="Genre 3"/>
+            <flux:select wire:model="genre1" label="Genre 1" placeholder="Choose genre...">
+                @forEach($genres as $genre)
+                    <flux:select.option value="{{$genre['id']}}"> {{$genre['name']}} </flux:select.option>
+                @endforeach
+            </flux:select>
+            <flux:select wire:model="genre2" label="Genre 2 (Optional)" placeholder="Choose genre...">
+                @forEach($genres as $genre)
+                    <flux:select.option value="{{$genre['id']}}"> {{$genre['name']}} </flux:select.option>
+                @endforeach
+            </flux:select>
+            <flux:select wire:model="genre3" label="Genre 3 (Optional)" placeholder="Choose genre...">
+                @forEach($genres as $genre)
+                    <flux:select.option value="{{$genre['id']}}"> {{$genre['name']}} </flux:select.option>
+                @endforeach
+            </flux:select>
         </div>
 
-        <flux:select label="Platform"/>
+        <flux:select wire:model="platform" label="Platform" placeholder="Choose platform...">
+            @forEach($platforms as $platform)
+                <flux:select.option value="{{$platform['id']}}"> {{$platform['name']}} </flux:select.option>
+            @endforeach
+        </flux:select>
 
         <flux:button type="submit">Search for Game</flux:button>
     </form>
